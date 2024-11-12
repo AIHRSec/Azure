f11ec7e63a74a0d8ff20338c41e4327a88073761
AIHR <50837811+AIHRSec@users.noreply.github.com>
2024-11-11 21:52:59 -0500

    Update configure_dc.ps1

--- Deployment/configure_dc.ps1
+++ Deployment/configure_dc.ps1
@@ -4,23 +4,23 @@ param (
     [string]$DomainNetbiosName = "IPT.NET",
     [string]$AdminName = "ipt_admin",
     [string]$ServerName = "ipt-dc",
-    [securestring]$AdminPassword
+    [securestring]$AdminPassword,
+    [securestring]$SafeModeAdminPassword
 )
 
 function Postreboot_ConfigureDC {
+
 New-Item -Path C:\Temp -Type Directory
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/AIHRSec/Azure/refs/heads/main/Deployment/postreboot_configure_dc.ps1 -OutFile C:\Temp\postreboot_configure_dc.ps1
 
 $scriptPath = "C:\Temp\postreboot_configure_dc.ps1"
 $scheduledTaskName = "ConfigureDomainPostReboot"
 
-# Save the post-reboot script to a file
-New-Item -Path $scriptPath -ItemType File -Force
-Set-Content -Path $scriptPath -Value $scriptContent
-
 # Create a scheduled task to run the post-reboot script after reboot
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File $scriptPath"
-$trigger = New-ScheduledTaskTrigger -Once -AtStartup -Delay "00:02:30"
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$trigger.Delay = "PT2M"
+$trigger.Repetition = "False" 
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -RunLevel Highest
 Register-ScheduledTask -TaskName $scheduledTaskName -Action $action -Trigger $trigger -Principal $principal
 
@@ -50,6 +50,6 @@ Install-ADDSForest `
     -SafeModeAdministratorPassword $AdminPassword `
     -InstallDNS `
     -Force
-Sleep -Seconds 45
+
 # Restart the server to complete AD DS installation
 Restart-Computer -Force
